007fe2755a1512c9913a747d9e8fddcb2ffe4c0e
CGuilhem <93581955+CGuilhem@users.noreply.github.com>
2023-02-07 22:39:34 +0100

    ✨ Add Ping command
    refacto

---
+++ main.js
@@ -0,0 +1,51 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(token);
+
+// Créez un horloge pour effectuer une action à une heure précise
+setInterval(() => {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours === 9 && minutes === 20) {
+    // Effectuez votre action ici
+    console.log('Action effectuée à 9h20');
+  }
+}, 60 * 1000); // Vérifiez toutes les minutes
+
+
