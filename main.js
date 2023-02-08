018c5277695b2a2f9c267b6bcd34f281a05b4231
Guilhem <guilhemcalonne@yahoo.fr>
2023-02-08 09:42:26 +0100

    🎨 Refactoring

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token, memberId } = require('./config.json');
+const { loadCommands, loadEvents } = require('./functions/loaders.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -13,26 +14,8 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
+loadCommands(client, commandsPath, commandFiles);
+loadEvents(client, eventsPath, eventFiles);
 
 setInterval(() => {
   let date = new Date();
