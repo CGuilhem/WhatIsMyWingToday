bfd23c7987a2bad977d2fde6397b4053679f73a6
Guilhem <guilhemcalonne@yahoo.fr>
2023-02-13 08:53:02 +0100

    🎨 Refactoring

--- functions/loaders.js
+++ functions/loaders.js
@@ -1,7 +1,11 @@
+const fs = require('node:fs');
 const path = require('node:path');
 
 
-const loadCommands = (client, commandsPath, commandFiles) => {
+const loadCommands = (client) => {
+    const commandsPath = path.join(__dirname, '../commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
@@ -14,7 +18,10 @@ const loadCommands = (client, commandsPath, commandFiles) => {
     }
 }
 
-const loadEvents = (client, eventsPath, eventFiles) => {
+const loadEvents = (client) => {
+    const eventsPath = path.join(__dirname, '../events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
     for (const file of eventFiles) {
         const filePath = path.join(eventsPath, file);
         const event = require(filePath);
