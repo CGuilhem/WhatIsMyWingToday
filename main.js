5158e711313cd96063a012490322b56766999277
Guilhem <guilhemcalonne@yahoo.fr>
2023-02-13 09:23:05 +0100

    refac

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token, memberId } = require('./config.json');
-const { loadCommands, loadEvents } = require('./functions/loaders.js');
+const fs = require("node:fs");
+
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { token, memberId } = require("./config.json");
+const { loadCommands, loadEvents } = require("./functions/loaders.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -17,21 +19,27 @@ setInterval(() => {
   if (hours === 09 && minutes === 20) {
     const guild = client.guilds.cache.first();
 
-    guild.members.fetch(memberId)
-      .then(member => {
+    guild.members
+      .fetch(memberId)
+      .then((member) => {
         if (!member) return console.error("Member not found");
 
-        const generalChannel = guild.channels.cache.find(channel => channel.name === 'général');
-        if (!generalChannel) return console.error('Channel not found');
+        const generalChannel = guild.channels.cache.find(
+          (channel) => channel.name === "général"
+        );
+        if (!generalChannel) return console.error("Channel not found");
 
         //Read names.json to extract the array called names
-        const names = fs.readFileSync('./names.json');
+        const names = fs.readFileSync("./names.json");
         const namesJson = JSON.parse(names);
 
         //Get a random name from the array
-        const randomName = namesJson.names[Math.floor(Math.random() * namesJson.names.length)];
+        const randomName =
+          namesJson.names[Math.floor(Math.random() * namesJson.names.length)];
 
-        generalChannel.send(`Aujourd'hui, Guilhem est de ${randomName.wing} et incarnera donc... ${randomName.name} !`);
+        generalChannel.send(
+          `Aujourd'hui, Guilhem est de ${randomName.wing} et incarnera donc... ${randomName.name} !`
+        );
         return member.setNickname(randomName.name);
       })
       .then(() => console.log("Pseudo modifié avec succès"))
